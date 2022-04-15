9231a9e538c0227566358b210ec32d547bbe8d63
BuYishi <bu-ish@qq.com>
2022-04-15 21:09:33 +0800

    Starts GifImageViewDemoActivity after btnGifImageViewDemo clicked

--- GifDemo/app/src/main/java/com/coolor/gif_demo/MainActivity.java
+++ GifDemo/app/src/main/java/com/coolor/gif_demo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.coolor.gif_demo;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.coolor.gif_demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.bind(findViewById(R.id.llRoot));
+        binding.btnGifTextViewDemo.setOnClickListener(v ->
+                GifTextViewDemoActivity.start(this));
+        binding.btnGifImageViewDemo.setOnClickListener(v ->
+                GifImageViewDemoActivity.start(this));
     }
 }
