3333c3085b8974d7b3face0bf4c6417238b0a3ca
BuYishi <bu-ish@qq.com>
2022-04-15 23:00:37 +0800

    Reformat code in GifImageViewDemoActivity.java

--- GifDemo/app/src/main/java/com/coolor/gif_demo/GifImageViewDemoActivity.java
+++ GifDemo/app/src/main/java/com/coolor/gif_demo/GifImageViewDemoActivity.java
@@ -14,7 +14,8 @@ public class GifImageViewDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif_image_view_demo);
-        ActivityGifImageViewDemoBinding binding = ActivityGifImageViewDemoBinding.bind(findViewById(R.id.llRoot));
+        ActivityGifImageViewDemoBinding binding =
+                ActivityGifImageViewDemoBinding.bind(findViewById(R.id.llRoot));
         binding.btnHalfSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(0.5f));
         binding.btnNormalSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(1));
         binding.btnDoubleSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(2));
