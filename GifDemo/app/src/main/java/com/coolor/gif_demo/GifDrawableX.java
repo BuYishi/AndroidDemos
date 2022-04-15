92d20ca5d8cf7e8fa253331fc45077f901b49151
BuYishi <bu-ish@qq.com>
2022-04-15 20:05:26 +0800

    Add SuppressWarnings annotation for field callback in GifDrawableX

--- GifDemo/app/src/main/java/com/coolor/gif_demo/GifDrawableX.java
+++ GifDemo/app/src/main/java/com/coolor/gif_demo/GifDrawableX.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import pl.droidsonroids.gif.GifDrawable;
 
 public class GifDrawableX extends GifDrawable {
+    @SuppressWarnings({"unused", "FieldCanBeLocal"})
     private final Callback callback;
 
     public GifDrawableX(@NonNull AssetManager assets, @NonNull String assetName, Callback callback) throws IOException {
