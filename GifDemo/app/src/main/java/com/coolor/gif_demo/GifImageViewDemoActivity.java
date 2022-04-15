0a467bfba3bf7466abcbf0a1133df45dd90be8c9
BuYishi <bu-ish@qq.com>
2022-04-15 22:58:24 +0800

    GifImageViewDemoActivity

---
+++ GifDemo/app/src/main/java/com/coolor/gif_demo/GifImageViewDemoActivity.java
@@ -0,0 +1,26 @@
+package com.coolor.gif_demo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.coolor.gif_demo.databinding.ActivityGifImageViewDemoBinding;
+
+public class GifImageViewDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gif_image_view_demo);
+        ActivityGifImageViewDemoBinding binding = ActivityGifImageViewDemoBinding.bind(findViewById(R.id.llRoot));
+        binding.btnHalfSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(0.5f));
+        binding.btnNormalSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(1));
+        binding.btnDoubleSpeed.setOnClickListener(v -> binding.givBeauty.setSpeed(2));
+    }
+
+    static void start(Context context) {
+        context.startActivity(new Intent(context, GifImageViewDemoActivity.class));
+    }
+}
